43c7343a7c6c1690d0b173f2634867a75031b8c2
seongwonlee <swlee@wisoft.io>
2017-11-08 10:46:33 +0900

    html code add

--- app.js
+++ app.js
@@ -10,9 +10,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use('/', express.static(__dirname + '/views'));
 
-// app.get('/', function (req, res) {
-//     res.sendFile((__dirname + '/views/login.html'));
-// });
 
 app.route('/')
     .get(function (req, res) {
@@ -22,11 +19,19 @@ app.route('/')
         res.send('check');
     });
 
-// app.use(function (req, res, next) {
-//     let err = new Error('Not Found');
-//     err.status(404);
-//     next(err);
-// });
+app.route('/main')
+    .get(function (req, res) {
+        res.sendFile((__dirname + '/views/main.html'))
+    });
+
+app.use(function (req, res, next) {
+    res.status(404).sendFile((__dirname + '/views/404_error.html'));
+});
+
+app.use(function (err, req, res, next) {
+    console.log(err.stack);
+    res.status(500).sendFile((__dirname + '/views/500_error.html'));
+});
 
 app.listen(8080, function () {
     console.log('Web server running at port 8080');
