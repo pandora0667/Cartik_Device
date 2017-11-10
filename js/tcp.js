4ef1c03fedc38b76330182333899c1b0778d38d6
seongwonlee <swlee@wisoft.io>
2017-11-10 13:53:54 +0900

    tcp error event error handler

--- js/tcp.js
+++ js/tcp.js
@@ -1,35 +1,46 @@
 'use strict';
 
 const net = require('net');
-const address = '203.230.100.59';
+const domain = require('domain');
+const errorHandler = domain.create();  // deprecate 되어 있으나 마땅한 대안이 존재하지 않음
+
+const address = '127.0.0.1';
 const port = 5001;
 
 let server = null;
+// state 정보를 가지고 확인했다가 타이머를 두고 재시도
+
 
 exports.getConnection = () => {
-    try {
-        server = net.connect({port: port, host: address}, function () {
-            console.log('remote server connection success!!');
-            server.setEncoding('utf-8');
 
-            server.on('data', function (data) {
-                console.log('From server : ', data);
-            });
-            server.on('end', function () {
-                console.log('Client disconnection');
-            });
-            server.on('error', function (err) {
-                console.log('Socket Error : ', JSON.stringify(err));
-                // TODO 서버가 연결되지 않을 때 재접속 시도 및 에러 핸들러 처리
-            });
-            server.on('timeout', function () {
-                console.log('Socket time out');
-            });
-            server.on('close', function () {
-                console.log('Socket Closed');
-            });
-        })
-    } catch (Exception) {
-        console.log('Remote server status error');
-    }
+    connection()
+        .then(function (status) {
+            console.log(status);
+        });
 };
+
+function connection() {
+    return new Promise(resolve => {
+        errorHandler.run(() => {
+            server = net.connect({port: port, host: address}, function () {
+                console.log('remote server connection success!!');
+                this.on('end', function () {
+                    console.log(connName + ' Client disconnected');
+                });
+                this.on('error', function (err) {
+                    console.log('Socket Error: ', JSON.stringify(err));
+                });
+                this.on('timeout', function () {
+                    console.log('Socket Timed Out');
+                });
+                resolve(true);
+            });
+        });
+
+        errorHandler.on('error', (err) => {
+            console.log('server not connection');
+            resolve(false);
+        });
+
+    });
+}
