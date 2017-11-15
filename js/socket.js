e298c9f8013779a14ae0bf22c8176b6e25953c80
seongwonlee <swlee@wisoft.io>
2017-11-16 00:21:41 +0900

    socket 2

--- js/socket.js
+++ js/socket.js
@@ -2,18 +2,26 @@
 
 const express = require('express');
 const app = express();
-const http = require('http').createServer(app).listen(3000, function () {
-    console.log('Web socket server running at 3000 port');
-});
-const io = require('socket.io').listen(http);
+// const http = require('http').createServer(app).listen(3000, function () {
+//     console.log('Web socket server running at 3000 port');
+// });
+// const io = require('socket.io').listen(http);
 
-io.sockets.on('connection', function (socket) {
+const server = require('http').createServer(app);
+server.listen(3000, () => {
+    console.log('listening on port 3000.')
+});
+const io = require('socket.io')(server);
+
+io.sockets.on('connection', (socket) =>  {
     console.log('--- Web socket connection!! ---');
     socket.emit('connected', 123);
 });
 
 exports.send = (sensor) => {
 	let msg = JSON.parse(sensor);
+	console.log('serial : ', msg.temp);
     io.socket.emit('mode', msg.mode);
 };
+
 
