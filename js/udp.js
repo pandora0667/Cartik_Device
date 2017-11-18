9dd302447eb7e2c83d80ba081f6d44d406d40283
seongwonlee <swlee@wisoft.io>
2017-11-18 15:07:50 +0900

    test2

--- js/udp.js
+++ js/udp.js
@@ -6,9 +6,9 @@ const client = dgram.createSocket('udp4');
 const host = 'jusk2.asuscomm.com';
 const port = 5001;
 
-exports.send = (sensor) => {
-
-    console.log('serial send : ', sensor.front);
+exports.send = (sensors) => {
+    let msg = JSON.parse(sensors);
+    console.log('serial send : ', msg.front);
     client.send(sensor, 0, sensor.length, port, host, (err, bytes) => {
         if (err) {
             throw  err;
