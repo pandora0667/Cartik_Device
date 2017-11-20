92488d6c04f1fadf8cc936c9b56b8aabaeaada04
seongwonlee <swlee@wisoft.io>
2017-11-20 22:36:38 +0900

    html add

--- routers/index.js
+++ routers/index.js
@@ -7,15 +7,31 @@ exports.web = (express, app) => {
     app.use(bodyParser.urlencoded({extended: false}));
 
     app.route('/')
-        .get((req, res) => {
-            res.redirect('/static/main.html');
+        .get(function (req, res) {
+            res.redirect('/static/testMain.html');
         });
 
-    app.use((req, res, next) => {
+    app.get('/car', (req, res) => {
+        res.sendFile((__dirname + '/static/testCarStatus.html'));
+    });
+
+    app.get('/status', (req, res) => {
+        res.sendFile((__dirname + '/static/internalStatus.html'));
+    });
+
+    app.get('/gps', (req, res) => {
+        res.sendFile((__dirname + '/static/map.html'));
+    });
+
+    app.get('/driver', (req, res) => {
+        res.sendFile((__dirname + '/static/driver.html'));
+    });
+
+    app.use(function (req, res, next) {
         res.status(404).redirect('/static//404_error.html');
     });
 
-    app.use((err, req, res, next) => {
+    app.use(function (err, req, res, next) {
         console.log(err.stack);
         res.status(500).redirect('/static/500_error.html');
     });
